603eb732db23c405118a995c1eaad12dbcf6a923
Sam Beller <sammbeller@gmail.com>
2024-10-08 18:19:06 -0400

    Test suite kind of working

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,15 @@
 import { type JestConfigWithTsJest, createDefaultEsmPreset } from "ts-jest";
+
 const defaultPreset = createDefaultEsmPreset();
 const jestConfig: JestConfigWithTsJest = {
   ...defaultPreset,
+  ...{
+    globalSetup: "jest-environment-puppeteer/setup",
+    globalTeardown: "jest-environment-puppeteer/teardown",
+    testEnvironment: "jest-environment-puppeteer",
+  }
 };
+
+console.log(defaultPreset, jestConfig);
 
 export default jestConfig;
--- test/registry.test.ts
+++ test/registry.test.ts
@@ -1,20 +1,27 @@
-import { SupportedPlayers, audioPlayerRegistry } from "../src/registry";
+import { SupportedPlayers, type audioPlayerRegistry } from "../src/registry";
 import { type AudioPlayer } from "../src/players/AudioPlayer";
 
 /**
  * These tests iterate over all supported audio players and run tests on each
  */
 describe("Registry Tests", () => {
+  let audioPlayerRegistryFromWindow: typeof audioPlayerRegistry;
+  beforeAll(async () => {
+    await page.goto('http://localhost:3000/test/registry.test.html');
+    audioPlayerRegistryFromWindow = await page.evaluate(() => (window as typeof window & {
+      audioPlayerRegistry: typeof audioPlayerRegistry
+    }).audioPlayerRegistry);
+    console.log(audioPlayerRegistryFromWindow);
+  });
   Object.values(SupportedPlayers).forEach((key) => {
     let audioPlayer: AudioPlayer;
     beforeEach(() => {
-      audioPlayer = audioPlayerRegistry[key]();
+      audioPlayer = audioPlayerRegistryFromWindow[key]();
     });
     describe(key, () => {
       describe("getDuration", () => {
         it("should return the duration of an audio file", async () => {
           await audioPlayer.load("../noise.wav");
-          console.log(audioPlayer.error);
           expect(await audioPlayer.getDuration()).toBe(10);
         });
       });
